e1304e09f36db36fb8faf7c348ad1f41ddda9a7d
Th3-Cod3 <th3-cod3-1230@hotmail.com>
2018-07-01 11:54:17 -0400

    Update

---
+++ app/controllers/PointCardController.php
@@ -0,0 +1,13 @@
+<?php  
+
+	namespace App\Controllers;
+
+	class PointCardController {
+		public function getIndex()
+		{
+			
+		}
+	}
+
+
+?>
---
+++ app/controllers/system/EventController.php
@@ -0,0 +1,21 @@
+<?php  
+
+	namespace App\Controllers\System;
+
+	class EventsController {
+		public function getIndex()
+		{
+			//get all comming events
+			time();
+			// $sql = "SELECT * FROM ipb_cal_events WHERE ";
+			// $pdo->query($sql);
+
+			//get all old events
+			
+
+			return time();
+		}
+	}
+
+
+?>
